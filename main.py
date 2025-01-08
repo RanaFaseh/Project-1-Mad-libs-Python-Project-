a6cb889384ad12716f9d33236f2a0fba66f12a7e
RanaFaseh <rana.faisal77@gmail.com>
2025-01-08 03:38:55 -0800

    Add files via upload
    this is a madlibs project which was given to me my institute PIAIC and i solved so here is its solution and as well as i am also providing you a colab link also here it is ...
    https://colab.research.google.com/drive/1P_9z0jggYc3LrRTSJxZaB0I8LJTewD9B#scrollTo=ChW6Fqg1RbxZ

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Assalam-O-Alikum")
+print("My Madlibs Topic is A Visit To Karachi Zoo")
+day = input(" Enter a day ") #Sunday
+place = input(" Enter a place ") #Karachi Zoo
+birds = input(" Enter two birds ") #Parrots & Peacocks 
+animal = input(" Enter a animal ") #Lion
+animal1 = input(" Enter a animal ") #Tiger
+animal2 = input(" Enter a animal ") #Elephant
+noun = input(" noun ") #animals
+bird1 = input(" Enter a bird ") #ducks 
+noun = input(" noun ") #nature & wildlife
+
+madlibs = f""" Last {day} I visited the {place} and 
+ had an amazing time. I saw colorful {birds}, {animal}, a 
+ {animal1}, and a {animal2} . The {noun} were playful,
+ and the {bird1} by the pond were floating . It was a delightful journey , filled 
+ with the beauty of {noun}! """
+
+print(madlibs)
